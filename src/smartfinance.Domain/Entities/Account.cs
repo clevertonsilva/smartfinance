b885e320ca303282be67193d4057b1267ea93b8a
TheBackstageDev <gabrielsuperzumb@gmail.com>
2024-06-05 14:11:10 -0300

    Implemented account Movements

--- src/smartfinance.Domain/Entities/Account.cs
+++ src/smartfinance.Domain/Entities/Account.cs
@@ -5,5 +5,6 @@ namespace smartfinance.Domain.Entities
     public class Account : EntityBase
     {
         public string Name { get; set; }
+        AccountMovement Account_Movement = new AccountMovement();
     }
 }
---
+++ src/smartfinance.Domain/Entities/Movement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smartfinance.Domain.Entities
+{
+    public enum MovementCategory
+    {
+        Bill,
+    };
+
+    public enum MovementType
+    {
+        Credit,
+        Debit
+    };
+    public struct Movement
+    {
+        public int Ammount { get; set; }
+        public string Description { get; set; }
+        public MovementCategory Category { get; set; }
+        public MovementType Type { get; set; }
+        public DateTime Date { get; set; }
+
+        public Movement(int Ammount, string Description, MovementCategory Category, MovementType Type, DateTime Date)
+        {
+            this.Type = Type;
+            this.Ammount = Ammount;
+            this.Description = Description;
+            this.Category = Category;
+            this.Date = Date;
+        }
+    }
+}
